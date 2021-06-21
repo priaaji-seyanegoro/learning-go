44ca6f5c3ab139d5af2007286bb847a0319ff974
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-06-21 10:02:40 +0700

    string , sort package

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// https://golang.org/pkg/ - this list of standart package on golang
 
 func main() {
-	fmt.Println("Hello First Golang Wish Me Luck")
+	greeting := "Hello Friends!!"
+	fmt.Println(strings.Contains(greeting, "hello"))          // methods to check variable contains something word - true/false
+	fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hai")) // methods for replace word
+	fmt.Println(strings.ToUpper(greeting))                    // uppercase word
+	fmt.Println(strings.Split(greeting, " "))                 // convert word to splice / arrays
+
+	sortHello := []string{"H", "E", "L", "L", "O"}
+	sort.Strings(sortHello)
+	fmt.Println("HELLO =>", sortHello)
 }
