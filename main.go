2bc5bbc668074f1c920e389b43c7d5b22c1e3d8f
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-06-21 10:36:56 +0700

    lopping

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello First Golang Wish Me Luck")
+	fmt.Println("Lopping lesson")
+
+	names := []string{"Aji", "Agus", "Hafiz"}
+
+	// for i := 1; i < len(names); i++ {
+	// 	fmt.Printf("The value of %v at index %v \n", names[i], i)
+	// }
+
+	for index, value := range names {
+		fmt.Printf("The value of %v at index %v \n", value, index)
+	}
 }
