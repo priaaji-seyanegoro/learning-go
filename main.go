69f336e89ed2821ca6968ab8d4645f7e60b354c6
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-06-21 15:16:06 +0700

    map type data

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+// map is type data on golang like obj on javascript but must have key and value same tipe data
+
 func main() {
-	fmt.Println("Hello First Golang Wish Me Luck")
+	menus := map[string]float64{
+		"soup":    6.33,
+		"chikens": 10.00,
+		"egg":     2.20,
+	}
+	fmt.Println(menus)
+	fmt.Println(menus["soup"])
+
+	for k, v := range menus {
+		fmt.Println(k, "-", v)
+	}
 }
