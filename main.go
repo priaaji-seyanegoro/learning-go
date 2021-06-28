9d9c23637aa5e76ba8417b2501cd567e207608cc
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-06-28 10:29:40 +0700

    anonym func

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type Blacklist func(string) bool
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("Your Blocked ", name)
+	} else {
+		fmt.Println("Welcome ", name)
+	}
+}
+
 func main() {
-	fmt.Println("Hello First Golang Wish Me Luck")
+	// you can declare to variable
+	blacklist := func(name string) bool {
+		return name == "admin"
+	}
+	registerUser("admin", blacklist)
+
+	//you can write function  on function
+	registerUser("Eko", func(name string) bool {
+		return name == "admin"
+	})
 }
