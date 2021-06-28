80944a6e7d60b01bfe8f917d8fc6445be62db095
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-06-28 09:34:14 +0700

    variadic function

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+func sumAll(number ...int) int {
+	//parameter berubah menjadi slice
+	total := 0
+	for _, value := range number {
+		total += value
+	}
+
+	return total
+}
+
 func main() {
-	fmt.Println("Hello First Golang Wish Me Luck")
+	// parameter pd sumAll terakhir bs menerima banyak parameter dgn 1 argumen
+	total := sumAll(10, 10, 10, 10, 10, 10)
+	fmt.Println(total)
+
+	//slice as parameters
+	numbers := []int{20, 20, 20, 20}
+	total2 := sumAll(numbers...)
+	fmt.Println(total2)
 }
