8265bce1bc5532297a731146e6de8bc4e59d2091
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2021-06-28 15:55:01 +0700

    interface

--- main.go
+++ main.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+type PersegiPanjang struct {
+	Panjang, Lebar int
+}
+
+type Persegi struct {
+	Sisi int
+}
+
+// group types together based on method
+type Luas interface {
+	CariLuas() int
+}
+
+func (num PersegiPanjang) CariLuas() int {
+	luas := num.Panjang * num.Lebar
+	return luas
+}
+
+func (num Persegi) CariLuas() int {
+	luas := num.Sisi * num.Sisi
+	return luas
+}
+
+func PrintHasil(name string, luas Luas) {
+	fmt.Printf("Luas %v : %v \n", name, luas.CariLuas())
+}
+
 func main() {
-	fmt.Println("Hello First Golang Wish Me Luck")
+	luasPersegi := Persegi{
+		Sisi: 10,
+	}
+
+	luasPersegiPanjang := PersegiPanjang{
+		Panjang: 10,
+		Lebar:   5,
+	}
+
+	PrintHasil("Persegi", luasPersegi)
+	PrintHasil("Persegi Panjang", luasPersegiPanjang)
 }
